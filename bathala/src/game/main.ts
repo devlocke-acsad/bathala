7911e23f571fce5fc35de9b0e9913bfcd4ab0b29
adiluexe <adiluexe@proton.me>
2025-09-04 23:35:10 +0800

    feat: add credits

--- bathala/src/game/main.ts
+++ bathala/src/game/main.ts
@@ -8,6 +8,7 @@ import { Shop } from "./scenes/Shop";
 import { Campfire } from "./scenes/Campfire";
 import { Treasure } from "./scenes/Treasure";
 import { Discover } from "./scenes/Discover";
+import { Credits } from "./scenes/Credits";
 import { DDADebugScene } from "./scenes/debug/DDADebugScene";
 import { AUTO, Game } from "phaser";
 import { Preloader } from "./scenes/Preloader";
@@ -20,7 +21,7 @@ const config: Phaser.Types.Core.GameConfig = {
   height: window.innerHeight,
   parent: "game-container",
   backgroundColor: "#150E10",
-  scene: [Boot, Preloader, MainMenu, Overworld, Combat, Shop, Campfire, Treasure, Discover, DDADebugScene, MainGame, GameOver],
+  scene: [Boot, Preloader, MainMenu, Overworld, Combat, Shop, Campfire, Treasure, Discover, Credits, DDADebugScene, MainGame, GameOver],
 
   // Keep antialias off for crisp rendering, but don't force pixelArt globally
   // render: {
